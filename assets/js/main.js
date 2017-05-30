8a25f87e048303339e489f1956908b2f60e6a45a
Kamil Weber <kamilweber24@gmail.com>
2017-05-30 20:29:17 +0200

    Refactor: Purely visual

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,27 +68,19 @@ var cursors;
 	}
 
   function render() {
-    //dragListener(game);
-  //  game.debug.cameraInfo(game.camera, 32, 32);
-  game.debug.text(game.time.fps, 32, 32, '#FFFFFF');
+    game.debug.text(game.time.fps, 32, 32, '#FFFFFF');
   }
 
   function update() {
-    if (cursors.up.isDown)
-    {
+    if (cursors.up.isDown) {
         game.camera.y -= 8;
-    }
-    else if (cursors.down.isDown)
-    {
+    } else if (cursors.down.isDown) {
         game.camera.y += 8;
     }
 
-    if (cursors.left.isDown)
-    {
+    if (cursors.left.isDown) {
         game.camera.x -= 8;
-    }
-    else if (cursors.right.isDown)
-    {
+    } else if (cursors.right.isDown) {
         game.camera.x += 8;
     }
   }
