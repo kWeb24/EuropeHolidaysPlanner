263b997d26716abeaddc5c58cd1d6a655bdc770f
Kamil Weber <kamilweber24@gmail.com>
2017-07-07 15:26:19 +0200

    Change: Remove config file and add example config file

--- db/Config.example.php
+++ db/Config.example.php
@@ -3,7 +3,9 @@
 class Config {
 
   const SQLITE_FILE = __DIR__ . DIRECTORY_SEPARATOR . 'sqlite/stats.db';
-
+  const API_PUBLIC = "yourUniquePublicKey";
+  const API_PRIVATE = "yourUniquePrivateKey";
+  
 }
 
 ?>
