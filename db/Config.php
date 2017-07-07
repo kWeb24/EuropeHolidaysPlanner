15cec5988b4df6fe29c23de820420ee88959cefc
Kamil Weber <kamilweber24@gmail.com>
2017-07-07 15:51:22 +0200

    Change: Restore previous config file

--- db/Config.php
+++ db/Config.php
@@ -3,9 +3,7 @@
 class Config {
 
   const SQLITE_FILE = __DIR__ . DIRECTORY_SEPARATOR . 'sqlite/stats.db';
-  const API_PUBLIC = "yourUniquePublicKey";
-  const API_PRIVATE = "yourUniquePrivateKey";
-  
+
 }
 
 ?>
